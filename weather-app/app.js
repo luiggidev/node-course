9724bc1aa6b33697c476631b5e88279762a9260e
luiggidev <luiggibellincanta@gmail.com>
2021-12-09 11:29:47 -0300

    Making HTTP Requests

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,11 +1,7 @@
-console.log('Starting')
+const request = require('request')
 
-setTimeout(() => {
-    console.log('2 Second Timer')
-}, 2000)
+const url = 'http://api.weatherstack.com/current?access_key=e84129bc20b99e3b1452c36379b4e3ff&query=42.3605,-71.0596&units=f'
 
-setTimeout(() => {
-    console.log('0 Second Timer')
-}, 0)
-
-console.log('Stopping')
+request({ url: url, json: true }, (error, response) => {
+    console.log(response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature + " degress out.")
+})
